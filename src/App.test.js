7e37ad1ef4d5bcc7778ae5f99cf2ece20bcd8c6c
heba <heba@gmail.com>
2022-08-07 20:33:19 -0700

    expose port 80

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('renders welcome message', () => {
+  render(<App />);
+  expect(screen.getByText('Learn React')).toBeInTheDocument();
 });
